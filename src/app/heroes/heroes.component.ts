7d35037da756452eeebbca964dfc2eb58a72021a
sing-u8 <singyu13@gmail.com>
2021-03-28 23:12:41 +0900

    4. add service -after adding observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MessageService} from '../message.service'
 import {HeroService} from './hero.service'
 import {Hero} from './hero'
 
@@ -12,7 +13,7 @@ export class HeroesComponent implements OnInit {
 
   heroes:Hero[];
   selectedHero: Hero;
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -20,9 +21,10 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 }
