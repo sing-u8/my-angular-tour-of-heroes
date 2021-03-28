b9939cf6f0966cadc35857282b73502471299fc8
sing-u8 <singyu13@gmail.com>
2021-03-28 22:21:08 +0900

    4. add service -before adding observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { HEROES } from './mock-heroes'
+import {HeroService} from './hero.service'
 import {Hero} from './hero'
 
 @Component({
@@ -10,14 +10,19 @@ import {Hero} from './hero'
 })
 export class HeroesComponent implements OnInit {
 
-  heroes:Hero[] = HEROES;
+  heroes:Hero[];
   selectedHero: Hero;
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
